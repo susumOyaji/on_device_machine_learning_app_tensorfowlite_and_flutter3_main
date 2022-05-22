d7df9f54456a0da552efc53635e20eabaa69f60f
susumOyaji <sumitomo0210@gmail.com>
2022-05-22 10:09:29 +0900

    up

---
+++ android/app/src/main/kotlin/com/example/on_device_machine_learning_app_tensorfowlite_and_flutter3_main/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.on_device_machine_learning_app_tensorfowlite_and_flutter3_main
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
